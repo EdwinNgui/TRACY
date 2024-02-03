6c411470ca7a40a77a87de6271a8bddc65660b67
Edwin <edwin.nguim@gmail.com>
2024-02-02 21:30:27 -0500

    Landing Page Core

--- qhacks/src/App.js
+++ qhacks/src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const Header = styled.header`
+  background-color: #333;
+  padding: 10px;
+  color: white;
+  text-align: left;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+`;
+
+const MainContent = styled.div`
+  flex: 1;
+  padding: 10%;
+  text-align: left;
+`;
+
+const SmallText = styled.p`
+  font-size: 40px;
+  color: #e6c910;
+  margin: 5px 0;
+`;
+
+const MassiveText = styled.p`
+  font-size: 140px;
+  font-weight: bold;
+  color: black;
+  margin: 20px 0;
+`;
+
+const DescriptionText = styled.p`
+  font-size: 36px;
+  color: black;
+  margin-bottom: 40px;
+`;
+
+const VideoInputSection = styled.div`
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+
+      {/* Landing Page */}
+      <Header>
+        <Title>TENNIS ANALYZER</Title>
+      </Header>
+      <MainContent>
+        <SmallText>BECOME A CHAMPION TODAY.</SmallText>
+        <MassiveText>
+          TENNIS GAMES ANALYZER
+        </MassiveText>
+        <DescriptionText>
+          Never lose another match to the same mistakes again. 
+        </DescriptionText>
+      </MainContent>
+
+      {/* Space */}
+      <div style={{ marginBottom: '50px' }} />
+
+      {/* Video Input Section */}
+      <VideoInputSection>
+        <h2>Upload Your Tennis Game Video (MP4)</h2>
+        <input type="file" accept="video/mp4" />
+        <p>Upload your tennis game video to start the analysis.</p>
+      </VideoInputSection>
+
+    </Container>
   );
 }
 
