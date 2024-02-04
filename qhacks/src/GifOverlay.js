772a3f1ed9801ae00896870e471f1bc8d60a5822
Edwin <edwin.nguim@gmail.com>
2024-02-04 03:32:12 -0500

    Opacity

--- qhacks/src/GifOverlay.js
+++ qhacks/src/GifOverlay.js
@@ -44,7 +44,7 @@ const GifOverlay = ({ showGif, handleShowGif }) => {
             left: 0,
             width: '100%',
             height: '100%',
-            backgroundColor: 'rgba(0, 0, 0, 0.8)',
+            backgroundColor: 'rgba(0, 0, 0, 0.9)',
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
